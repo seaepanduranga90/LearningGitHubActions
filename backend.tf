b5162c5c0b8f02ba6ae200c249b51a27c92d0817
seaepanduranga90 <95756363+seaepanduranga90@users.noreply.github.com>
2024-01-15 10:45:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-tf-test-bucket876713"
-    key = "my-tf-test-bucket876713/terraform.tfstate"
+    bucket = "terraformbucket12344"
+    key = "terraformbucket12344/terraform.tfstate"
     region = "ap-south-1"
     workspace_key_prefix = "Test"
   }
